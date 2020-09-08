5e905e77e7d5b0a7efa677582471a57aec95a8cc
tschaffter <thomas.schaffter@gmail.com>
2020-09-08 21:39:22 +0000

    Clean up data node openapi.yml

--- openapi/data-node/generate-flask.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-# Requires openapi-generator to be installed
-# brew install openapi-generator on a mac
-#
-#
-openapi-generator generate -i openapi.yaml -g python-flask -o ~/PycharmProjects/openapi-flask/
